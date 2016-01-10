2a57c669d739ea8ac8936b7bb24b52e4a10a3ab1
The Invisible Man <cgranados@delivery.com>
2016-01-10 13:43:31 -0500

    Initial Commit

---
+++ Star Tracker/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Star Tracker
+//
+//  Created by Carlos Granados on 1/10/16.
+//  Copyright © 2016 Ragnarok Industries. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
